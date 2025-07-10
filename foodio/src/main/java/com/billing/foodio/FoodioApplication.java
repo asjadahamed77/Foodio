2e90035dd7ac026cdf1e74b073f6b18a74f37522
asjadahamed77 <ahamedasjad77@gmail.com>
2025-07-10 19:32:23 +0530

    Initialized the project

---
+++ foodio/src/main/java/com/billing/foodio/FoodioApplication.java
@@ -0,0 +1,13 @@
+package com.billing.foodio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodioApplication.class, args);
+	}
+
+}
