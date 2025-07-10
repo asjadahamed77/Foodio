934964361929cdc661345373665cd077e4719922
asjadahamed77 <ahamedasjad77@gmail.com>
2025-07-10 19:56:50 +0530

    Setup backend spring boot

---
+++ backend/src/test/java/com/asjadahd/billing/BillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asjadahd.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
