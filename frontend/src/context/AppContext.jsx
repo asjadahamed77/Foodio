5f753b40275c807f9aaabd3908b89ae9f16ab524
asjadahamed77 <ahamedasjad77@gmail.com>
2025-07-16 21:53:22 +0530

    Working on creating cartItems

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,6 +7,18 @@ export const AppContext = createContext(null);
 export const AppContextProvider = (props) => {
   const [categories, setCategories] = useState([]);
   const [itemsData, setItemsData] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item)=> {
+    const existingItem = cartItems.find(cartItem => cartItem.name === item.name);
+    if(existingItem){
+      setCartItems(cartItems.map(cartItem => cartItem.name === item.name ? {...cartItem, quantity: cartItem.quantity + 1} : cartItem));
+    }
+    else{
+      setCartItems([...cartItems, {...item, quantity: 1}]); 
+    }
+
+  }
 
   const [auth, SetAuth] = useState({
     token: null,
