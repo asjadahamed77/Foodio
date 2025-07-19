ecf7c87198920c381dd12a54c1db39064cf27308
asjadahamed77 <ahamedasjad77@gmail.com>
2025-07-19 17:04:07 +0530

    Working on Orders

---
+++ billingSoftware/src/main/java/lk/asjad/billingSoftware/entity/OrderEntity.java
@@ -0,0 +1,55 @@
+package lk.asjad.billingSoftware.entity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lk.asjad.billingSoftware.io.PaymentDetails;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tbl_orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderId;
+    private String customerName;
+    private String phoneNumber;
+    private Double totalAmount;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(cascade=CascadeType.ALL, orphanRemoval=true)
+    @JoinColumn(name = "order_id")
+    private List<OrderItemEntity> items = ArrayList<>();
+
+    private PaymentDetails paymentDetails;
+
+    @PrePersist
+    protected void onCreate() {
+        this.orderId = "ORD-" + UUID.randomUUID().toString();
+        this.createdAt = LocalDateTime.now();
+    }
+}
